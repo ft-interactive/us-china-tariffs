4365048f2bc37cce3eb286443595b6c9317b4800
Cale Tilford <cale.tilford@ft.com>
2018-06-21 12:00:20 +0100

    Fix jump to section behaviour

--- client/index.js
+++ client/index.js
@@ -5,7 +5,8 @@ const timelineDots = document.querySelectorAll('.timeline__circle');
 window.addEventListener('scroll', () => {
   const containerHeight =
     document.querySelector('#timeline-wrapper').getBoundingClientRect().bottom -
-    document.querySelector('#timeline-wrapper').getBoundingClientRect().top;
+    document.querySelector('#timeline-wrapper').getBoundingClientRect().top +
+    80;
 
   const containerPosition = document.querySelector('#timeline-wrapper').offsetTop + containerHeight;
 
@@ -19,7 +20,14 @@ window.addEventListener('scroll', () => {
 Array.from(timelineDots).forEach((timelineDot) => {
   timelineDot.addEventListener('click', () => {
     const id = timelineDot.dataset.cardId;
-    const yPos = document.querySelector(`#tariffs-${id}`).scrollIntoView();
+    const yPos =
+      document.querySelector(`#tariffs-${id}`).getBoundingClientRect().top +
+      window.scrollY -
+      84 -
+      20;
+
+    console.log(yPos);
+    window.scrollTo(0, yPos);
 
     const textDate = timelineDot.querySelector('.timeline__circle__text-date').innerText;
   });
