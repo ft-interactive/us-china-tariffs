dddf0526aa9ae6dc3718816c8c56519b1a1c179a
Cale Tilford <cale.tilford@ft.com>
2018-06-22 15:03:11 +0100

    Use scrollama for timeline scrolling

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import './styles.scss';
+import scrollama from 'scrollama';
 
 const timelineDots = document.querySelectorAll('.timeline__circle');
 
@@ -30,4 +31,48 @@ Array.from(timelineDots).forEach((timelineDot) => {
 
     window.scrollTo(0, yPos);
   });
+});
+
+const circles = document.querySelectorAll('.timeline__circle');
+
+function clearCirclesExcept(id) {
+  circles.forEach((circle) => {
+    if (circle.dataset.cardId !== id) {
+      circle.classList.remove('selected');
+    }
+  });
+}
+
+const scroller = scrollama();
+
+scroller
+  .setup({
+    step: '.date-step',
+    offset: 0.2,
+    progress: true,
+  })
+  .onStepEnter((trigger) => {
+    document.querySelector('#timeline-container').classList.add('tacked');
+    document.querySelector('.timeline-space').style.height = '84px';
+
+    const id = trigger.element.dataset.cardId;
+    const circle = document.querySelector(`.timeline__circle[data-card-id="${id}"`);
+    circle.classList.add('selected');
+    clearCirclesExcept(id);
+  })
+  .onStepExit((trigger) => {
+    const id = trigger.element.dataset.cardId;
+    const circle = document.querySelector(`.timeline__circle[data-card-id="${id}"`);
+    if (
+      trigger.index !== 0 &&
+      trigger.direction !== 'up' &&
+      (trigger.index !== circles.length - 1 && trigger.direction !== 'down')
+    ) {
+      console.log(trigger);
+      circle.classList.remove('selected');
+    }
+  });
+
+window.addEventListener('resize', () => {
+  scrollama.resize();
 });
